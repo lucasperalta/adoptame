e8a20c43a6b49c85803f997aab352122e70a2007
Lucas <lucas@C1MNT7R0DTY3.local>
2019-08-22 19:50:04 -0300

    login mobile

---
+++ src/main/java/ar/edu/davinci/adoptame/controller/mobile/LoginMobile.java
@@ -0,0 +1,61 @@
+package ar.edu.davinci.adoptame.controller.mobile;
+
+import ar.edu.davinci.adoptame.DTO.UsuarioDTO;
+import ar.edu.davinci.adoptame.domain.Usuario;
+import ar.edu.davinci.adoptame.exception.NotFoundException;
+import ar.edu.davinci.adoptame.service.EstadoService;
+import ar.edu.davinci.adoptame.service.LoginService;
+import ar.edu.davinci.adoptame.service.RolService;
+import ar.edu.davinci.adoptame.service.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Map;
+
+@Controller    // This means that this class is a Controller
+@RequestMapping(path="/mobile") // This means URL's start with /demo (after Application path)
+public class LoginMobile {
+
+    @Autowired
+    private LoginService loginService;
+
+    @Autowired
+    private RolService rolService;
+
+    @Autowired
+    private UsuarioService usuarioService;
+
+    @Autowired
+    private EstadoService estadoService;
+
+    @PostMapping("/ingresarMobile")
+    public @ResponseBody
+    UsuarioDTO findUser(HttpServletRequest request,
+                        HttpServletResponse response,@RequestBody Map<String, Object> params) {
+        Usuario usuarioExiste = null;
+        String usuario= (String)params.get("usuario");
+        String password= (String)params.get("password");
+        try {
+
+
+            usuarioExiste = loginService.buscarUsuarioByEmail(usuario);
+        } catch (NotFoundException e) {
+
+            return new UsuarioDTO();
+        }
+
+
+        UsuarioDTO usuarioDTO = new UsuarioDTO();
+        if (usuarioExiste.getPassword().equals(password)) {
+            usuarioDTO.setEmail(usuarioExiste.getEmail());
+            usuarioDTO.setPassword(usuarioExiste.getPassword());
+            usuarioDTO.setNombre(usuarioExiste.getNombre());
+            usuarioDTO.setApellido(usuarioExiste.getApellido());
+            usuarioDTO.setEstado(usuarioExiste.getEstado().getEstado());
+        }
+        return usuarioDTO;
+    }
+}
