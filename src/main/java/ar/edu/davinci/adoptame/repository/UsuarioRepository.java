26237d2c70e3cc343271c3a4c389a81f2261f393
Lucas <lucas@C1MNT7R0DTY3.local>
2020-08-29 22:18:53 -0300

    mascotas disponibles monile sin filtro, sigin usuario upload de mascotas

--- src/main/java/ar/edu/davinci/adoptame/repository/UsuarioRepository.java
+++ src/main/java/ar/edu/davinci/adoptame/repository/UsuarioRepository.java
@@ -7,8 +7,6 @@ import org.springframework.data.repository.query.Param;
 
 //import org.springframework.data.repository.CrudRepository;
 
-// This will be AUTO IMPLEMENTED by Spring into a Bean called personaRepository
-// CRUD refers Create, Read, Update, Delete
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
 
