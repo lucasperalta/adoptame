385e2963df7755a12604cb212790a7680a07bc2a
Lucas <lucas@C1MNT7R0DTY3.local>
2020-10-24 19:58:53 -0300

    cambio email

--- src/main/java/ar/edu/davinci/adoptame/service/EmailServiceImpl.java
+++ src/main/java/ar/edu/davinci/adoptame/service/EmailServiceImpl.java
@@ -1,11 +1,14 @@
 package ar.edu.davinci.adoptame.service;
 
 import ar.edu.davinci.adoptame.constantes.Constantes;
+import com.sendgrid.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
+
+import java.io.IOException;
 
 @Service
 public class EmailServiceImpl implements EmailService {
@@ -20,15 +23,31 @@ public class EmailServiceImpl implements EmailService {
      * @param text
      */
     @Override
-    public void sendSimpleMessage(String to, String subject, String text) {
+    public void sendSimpleMessage(String to, String subject, String text) throws IOException {
         try {
-            SimpleMailMessage message = new SimpleMailMessage();
-            message.setFrom(Constantes.FROM_ADDRESS);
-            message.setTo(to);
-            message.setSubject(subject);
-            message.setText(text);
 
-            emailSender.send(message);
+
+            Email from = new Email(Constantes.FROM_ADDRESS);
+
+            Email to2 = new Email(to);
+            Content content = new Content("text/plain", text);
+            Mail mail = new Mail(from, subject, to2, content);
+
+            SendGrid sg = new SendGrid("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");
+            Request request = new Request();
+            try {
+                request.setMethod(Method.POST);
+                request.setEndpoint("mail/send");
+                request.setBody(mail.build());
+                Response response = sg.api(request);
+                System.out.println(response.getStatusCode());
+                System.out.println(response.getBody());
+                System.out.println(response.getHeaders());
+            } catch (IOException ex) {
+                throw ex;
+            }
+
+
         } catch (MailException exception) {
             exception.printStackTrace();
         }
